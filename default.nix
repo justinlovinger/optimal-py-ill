e2db895ebbb02c749b59edbb95e416c07a365727
Justin Lovinger <git@justinlovinger.com>
2020-09-20 21:29:19 -0400

    Add `checkPhase`

--- default.nix
+++ default.nix
@@ -8,8 +8,8 @@ let
   learning = import (pkgs.fetchFromGitHub {
     owner = "JustinLovinger";
     repo = "learning";
-    rev = "5ae4136173d0fd0e903301bb27be87459512cd40";
-    sha256 = "13nf3xjd48h4n15i4i5vz3cqvpzsj0zlvn6g1qr1l5jmk6h6n1a3";
+    rev = "4a40e39989f5234affeddd3ef7d5fd6a59784041";
+    sha256 = "1yh7y09k6q8c54r4gz31dccbc5vizl1na7fvpah043kzfj8klf5a";
   }) { inherit pkgs; };
 in pkgs.python2Packages.buildPythonPackage {
   pname = "ill";
@@ -18,6 +18,8 @@ in pkgs.python2Packages.buildPythonPackage {
 
   checkInputs = with pkgs.python2Packages; [ pytest ];
   propagatedBuildInputs = with pkgs.python2Packages; [ learning numpy scipy ];
+
+  checkPhase = "pytest";
 
   meta = with pkgs.stdenv.lib; {
     description = "Ensemble that enables supervised learning algorithms to effectively learn incrementally";
